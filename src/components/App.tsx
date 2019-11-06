e1cb6e7e77f59df3624ffaf282667d15186ee2ab
kiliman <kiliman@gmail.com>
2019-11-06 11:16:05 -0500

    💄 Render todos as unordered list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import logo from '../logo.svg'
-import './App.css'
+import { useOvermind } from '../overmind'
 
 const App: React.FC = () => {
+  const { state } = useOvermind()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {Object.values(state.todos).map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
   )
 }
 
